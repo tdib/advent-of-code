6b6547ec9b8fd9afb2c54e4d73625e059290ba8f
s3838765 <s3838765@student.rmit.edu.au>
2022-12-08 22:13:26 +1100

    Add progress to part 2

--- day8/day8b.py
+++ day8/day8b.py
@@ -15,25 +15,17 @@ for row, line in enumerate(lines):
   for col, tree in enumerate(line):
     # Outer trees
     if row == 0 or col == 0 or row == len(lines)-1 or col == len(line)-1:
-      print(1, (row, col))
       visible.add((row, col))
-      # print(row, col)
     elif tree > tallest_tree:
-      
       tallest_tree = tree
-      print(2, (row, col), 'tree', tree, 'tallest', tallest_tree)
-      print(len(visible))
       visible.add((row, col))
-      print(len(visible))
 
 # Right to left
 for row, line in enumerate(lines):
   tallest_tree = line[-1]
   for col, tree in enumerate(line[::-1]):
-    print('??', tree, tallest_tree)
     if tree > tallest_tree:
       tallest_tree = tree
-      print(3, (row, len(line)-col-1))
       visible.add((row, len(line)-col-1))
 
 lines_transposed = [list(x) for x in zip(*lines)]
@@ -43,7 +35,6 @@ for row, line in enumerate(lines_transposed):
   for col, tree in enumerate(line):
     if tree > tallest_tree:
       tallest_tree = tree
-      print(4, (col, row))
       visible.add((col, row))
 
 # Right to left
@@ -52,7 +43,6 @@ for row, line in enumerate(lines_transposed):
   for col, tree in enumerate(line[::-1]):
     if tree > tallest_tree:
       tallest_tree = tree
-      print(5, (len(line)-col-1, row))
       visible.add((len(line)-col-1, row))
 
 
@@ -65,14 +55,13 @@ def get_scenic_score(tree_pos):
   bot_diff = len(lines) - r - 1
   left_diff = c
   right_diff = len(lines[0]) - c - 1
-  view_score = 1
-  print(top_diff, left_diff, bot_diff, right_diff)
 
   total_up = 0
   for i in range(1, top_diff+1):
     # if len(tops) == 0 or lines[r-i][c] > tops[-1]:
     #   tops.append(lines[r-i][c])
     if not lines[r-i][c] > lines[r][c]:
+      print('up', lines[r-i][c])
       total_up += 1
     else:
       break
@@ -80,6 +69,7 @@ def get_scenic_score(tree_pos):
   total_down = 0
   for i in range(1, bot_diff+1):
     if not lines[r+i][c] > lines[r][c]:
+      print('down', lines[r+i][c])
       total_down += 1
     else:
       break
@@ -87,6 +77,7 @@ def get_scenic_score(tree_pos):
   total_left = 0
   for i in range(1, left_diff+1):
     if not lines[r][c-i] > lines[r][c]:
+      print('left', lines[r][c-i])
       total_left += 1
     else:
       break
@@ -95,16 +86,13 @@ def get_scenic_score(tree_pos):
   total_right = 0
   for i in range(1, right_diff+1):
     if not lines[r][c+i] > lines[r][c]:
+      print('right', lines[r][c+i])
       total_right += 1
     else:
       break
 
-  # print('top', tops)
-  # print('bot', bots)
-  # print('left', lefts)
-  # print('right', rights)
-  # hi = [1, len(tops), len(bots), len(lefts), len(rights)]
-  # print('!!!!!!!', r, c, prod(hi))
+  x = prod([total_up, total_down, total_left, total_right])
+  print(f'Pos {tree_pos}, u={total_up} d={total_down} l={total_left} r={total_right} x={x}')
   # return prod(hi)
 
   return prod([total_up, total_down, total_left, total_right])
