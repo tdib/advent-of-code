9a994416f9241db1dabcdfcf9effb95a7e6a85d1
tdib <github@tdib.xyz>
2023-11-19 21:59:26 +1100

    Finish day 13 part 2 in python, clean up code

--- day13/day13.py
+++ day13/day13.py
@@ -3,73 +3,60 @@ from itertools import zip_longest
 with open('/Users/dib/dev/advent-of-code-2022/day13/input.txt', 'r') as f:
   lines = list(map(str.strip, f.readlines()))
 
+# Compares two elements, returning true if they are in the correct order,
+# and false otherwise
 def recursive_compare(l, r):
-  print(type(l), l)
-  print(type(r), r)
+  # One of the elements is an int and we must convert it to a list
+  if type(l) == list and type(r) == int: r = [r]
+  elif type(l) == int and type(r) == list: l = [l]
 
-  if type(l) == list and type(r) == int:
-    print(l, r, 'converting r')
-    r = [r]
-  elif type(l) == int and type(r) == list:
-    print(l, r, 'converting l')
-    l = [l]
-
-  # Left ran out of elements before right
-  if l is None and r is not None:
-    print('Correct order - mixed')
-    return True
-  # Right ran out of elements before left
-  if l is not None and r is None:
-    print('Incorrect order - mixed')
-    return False
+  # Left ran out of elements before right - correct order
+  if l is None and r is not None: return True
+  # Right ran out of elements before left - incorrect order
+  if l is not None and r is None: return False
 
   # Both lists
   if type(l) == list and type(r) == list:
-    print(l, r, 'both lists')
-    # recursive_compare(*l, *r)
-    if l == [] and r == []:
-      return
     for l, r in zip_longest(l, r):
-      print(':)', l, r, type(l), type(r))
-      x = recursive_compare(l, r)
-      if x != None:
-        return x
+      res = recursive_compare(l, r)
+      if res != None: return res
     
   # Both ints
   if type(l) == int and type(r) == int:
-    print(l, r, 'both ints')
-    if l < r:
-      print('Correct order - int')
-      return True
-    elif l == r:
-      print('Indeterminate order - int')
-      return
-    else:
-      print('Incorrect order - int')
-      return False
+    if l < r: return True
+    elif l > r: return False
 
-
+### Part 1 ###
 chunks = [lines[i:i+2] for i in range(0, len(lines), 3)]
 indices = []
 for idx, chunk in enumerate(chunks):
   left = eval(chunk[0])
   right = eval(chunk[1])
-
   for l, r in zip_longest(left, right):
     res = recursive_compare(l, r)
-    # res = compare(l, r)
-    if res == True:
-      indices.append(idx+1)
-    if res != None:
-      print('res', res)
-      print('-------------')
-      break
+    if res == True: indices.append(idx+1)
+    if res != None: break
+
+print(f'Part 1 answer: {sum(indices)}')
+##############
 
 
+### Part 2 ###
+DISTRESS_1 = [[2]]
+DISTRESS_2 = [[6]]
+packets = [eval(line) for line in lines if line] + [DISTRESS_1, DISTRESS_2]
 
-    print()
-  print()
+# Sort the packets, using recursive_compare as the comparison function
+is_sorted = False
+while not is_sorted:
+  is_sorted = True
+  for i in range(len(packets) - 1):
+    # Current and next element are not in order - swap them
+    if not recursive_compare(packets[i], packets[i+1]):
+      packets[i], packets[i+1] = packets[i+1], packets[i]
+      is_sorted = False
 
-print(indices)
-print(len(indices))
-print(sum(indices))
+distress_idx_1 = packets.index(DISTRESS_1) + 1
+distress_idx_2 = packets.index(DISTRESS_2) + 1
+print(f'Part 2 answer: {distress_idx_1 * distress_idx_2}')
+##############
