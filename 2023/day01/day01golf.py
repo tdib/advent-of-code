270b2d9ca5193f29e7041f0ade824524491cca99
tdib <github@tdib.xyz>
2023-12-01 22:46:03 +1100

    Reduce golf char count 330->304

--- 2023/day01/day01golf.py
+++ 2023/day01/day01golf.py
@@ -1,3 +1,3 @@
-import re;a=0;b=0;n=["one","two","three","four","five","six","seven","eight","nine"];p=f"(?=(\\d|{'|'.join(n)}))"
-for l in open("input.txt").readlines():r=re.findall(r"\d",l);a+=int(r[0]+r[-1]);r=re.findall(p,l);b+=int(f"{r[0]if r[0].isdigit()else n.index(r[0])+1}{r[-1]if r[-1].isdigit()else n.index(r[-1])+1}")
-print(a);print(b)
+import re;a=b=0;n="one two three four five six seven eight nine".split()
+for l in open("input.txt").readlines():r=re.findall(r"\d",l);a+=int(r[0]+r[-1]);r=re.findall(f"(?=(\\d|{'|'.join(n)}))",l);x,y=r[0],r[-1];b+=int(f"{x if x.isdigit()else n.index(x)+1}{y if y.isdigit()else n.index(y)+1}")
+print(a,b)
