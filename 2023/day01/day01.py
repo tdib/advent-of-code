f22645dd930f3af54f38c8c29b13d7b43fe45c06
tdib <github@tdib.xyz>
2023-12-02 00:44:31 +1100

    Clean up and document code

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -11,6 +11,7 @@ def solve_part_1():
         matches = re.findall(r"\d", line)
         # Check just to ensure no errors happen with inputs not containing numbers (i.e. test data)
         if matches:
+            # Both matches will be strings, so we can use + to concatenate them and then cast to int
             ans += int(matches[0] + matches[-1])
     return ans
 
@@ -19,16 +20,25 @@ def solve_part_2():
     ans = 0
     nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     for line in lines:
-        # Find the digit numbers
+        # Find all instances of any of the digit strings, or real digits
         matches = re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)
 
+        # Find the leftmost and rightmost digits/strings by using the matches found above
         l = matches[0]
         r = matches[-1]
 
+        # If we have a string like "one", we must convert it to its value. We do this by using
+        # the array index + 1 (to offset for 0-based indexing), e.g. "one" is at index 0, "two"
+        # is at 1, and so on.
         l = int(l) if l.isdigit() else nums.index(l) + 1
         r = int(r) if r.isdigit() else nums.index(r) + 1
 
-        ans += int(f"{l}{r}")
+        # We can use this method if we have two ints. This essentially takes the leftmost value
+        # and places it in the 10s column where it belongs, and then adds the rightmost (in the ones)
+        ans += 10 * l + r
+        # We can use this method if we have not casted our values to ints,
+        # e.g. if we had something like "1" as opposed to 1
+        # ans += int(f"{l}{r}")
 
     return ans
 
