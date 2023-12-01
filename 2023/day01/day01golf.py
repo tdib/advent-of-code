f5eed2d0b82bbf6fa006966b6a4de2942e36042a
tdib <github@tdib.xyz>
2023-12-02 01:33:29 +1100

    Improve golf 303 -> 301

--- 2023/day01/day01golf.py
+++ 2023/day01/day01golf.py
@@ -1,3 +1,3 @@
-import re;a=b=0;n="one two three four five six seven eight nine".split()
-for l in open("input.txt").readlines():r=re.findall(r"\d",l);a+=int(r[0]+r[-1]);r=re.findall(f"(?=(\\d|{'|'.join(n)}))",l);x,y=r[0],r[-1];b+=int(f"{x if x.isdigit()else n.index(x)+1}{y if y.isdigit()else n.index(y)+1}")
+import re;a=b=0;n="_ one two three four five six seven eight nine".split()
+for l in open("input.txt").readlines():r=re.findall(r"\d",l);a+=int(r[0]+r[-1]);r=re.findall(f"(?=(\\d|{'|'.join(n)}))",l);x,y=r[0],r[-1];b+=int(f"{x if x.isdigit()else n.index(x)}{y if y.isdigit()else n.index(y)}")
 print(a,b)
