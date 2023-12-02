f110c44adaf2108a2508022ea707e611ee268791
tdib <github@tdib.xyz>
2023-12-02 23:13:47 +1100

    Reduce golf 301 -> 282

--- 2023/day01/day01golf.py
+++ 2023/day01/day01golf.py
@@ -1,3 +1,3 @@
-import re;a=b=0;n="_ one two three four five six seven eight nine".split()
-for l in open("input.txt").readlines():r=re.findall(r"\d",l);a+=int(r[0]+r[-1]);r=re.findall(f"(?=(\\d|{'|'.join(n)}))",l);x,y=r[0],r[-1];b+=int(f"{x if x.isdigit()else n.index(x)}{y if y.isdigit()else n.index(y)}")
+import re;a=b=0;n="_ one two three four five six seven eight nine".split();F=lambda y:int(y) if y.isdigit()else n.index(y);G=re.findall
+for l in open("input.txt").readlines():r=G(r"\d",l);a+=int(r[0]+r[-1]);r=G(f"(?=(\\d|{'|'.join(n)}))",l);x,y=r[0],r[-1];b+=10*F(x)+F(y)
 print(a,b)
