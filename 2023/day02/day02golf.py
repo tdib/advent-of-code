b1959b734a602104e90062d8fde70bc12e13ba59
tdib <github@tdib.xyz>
2023-12-03 00:38:26 +1100

    Golf day 2 in python (270)

---
+++ 2023/day02/day02golf.py
@@ -0,0 +1,7 @@
+a=0;p=[]
+for i,l in enumerate(open("input.txt").readlines()):
+ c=[0]*3
+ for g in l.split(":")[1].split(";"):
+  for e in g.split(","):n,x=e.split();x=len(x)-3;c[x]=max(c[x],int(n))
+ a+=c[0]*c[1]*c[2];p.append((i+1)*all(c[x]<=(12,13,14)[x]for x in(0,1,2)))
+print(sum(p),a)
