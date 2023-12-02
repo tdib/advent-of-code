9e1acb1850fa67142fd4067ff141a57b82c449fc
tdib <github@tdib.xyz>
2023-12-02 13:23:17 +1100

    Add some comments

--- 2023/day01/rust/src/main.rs
+++ 2023/day01/rust/src/main.rs
@@ -1,6 +1,10 @@
 use fancy_regex::{Captures, Regex};
 use std::fs::read_to_string;
 
+const NUMS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
 fn main() {
     let input: String = read_to_string("../../input.txt").expect("Input file not found");
 
@@ -8,6 +12,11 @@ fn main() {
     println!("Part 2 answer: {}", solve_part_2(&input));
 }
 
+/// Generic solve method that finds all regex matches in the input,
+/// and sums the first and last matches for each line.
+/// *Note: The regex must contain a positive lookahead to work properly*
+///
+/// The sum of the first and last number of each line, across all lines
 fn solve(input: &str, pattern: Regex) -> usize {
     let mut ans = 0;
     for line in input.lines() {
@@ -15,7 +24,9 @@ fn solve(input: &str, pattern: Regex) -> usize {
 
         // Check to ensure all types of inputs work
         if !caps.is_empty() {
+            // Leftmost match
             let l = parse_capture(caps.first());
+            // Rightmost match
             let r = parse_capture(caps.last());
             ans += 10 * l + r;
         }
@@ -25,26 +36,28 @@ fn solve(input: &str, pattern: Regex) -> usize {
 }
 
 fn solve_part_1(input: &str) -> usize {
-    // Positive lookahead is not strictly necessary for the problem, but is included to make the
-    // generic parse method work
+    // Match any digits in a string
+    // Note: positive lookahead is not strictly necessary for the problem,
+    // but is included to make the generic parse method work
     let re = Regex::new(r"(?=(\d))").unwrap();
     solve(input, re)
 }
 
 fn solve_part_2(input: &str) -> usize {
+    // Match any digit or string representation of a digit
     let re = Regex::new(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))").unwrap();
     solve(input, re)
 }
 
-const NUMS: [&str; 9] = [
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-];
+/// Given a captures_iter option, parse it such that we return a usize of the digit matched
+/// (e.g. "1" -> 1), or a usize corresponding to a string representation (e.g. "one" -> 1)
 fn parse_capture(cap: Option<&Result<Captures, fancy_regex::Error>>) -> usize {
     cap.unwrap()
         .as_ref()
         .unwrap()
         .get(1)
         .map(|m| {
+            // Check if we have a digit like "1", or a string representation like "one"
             let m = m.as_str();
             if NUMS.contains(&m) {
                 Ok(NUMS.iter().position(|item| item == &m).unwrap() + 1)
