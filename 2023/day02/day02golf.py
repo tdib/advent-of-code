cd54bdc8e5fc5fa7c00097472fec7e0978876ce7
tdib <github@tdib.xyz>
2023-12-03 00:40:59 +1100

    Reduce by one character 270 -> 269

--- 2023/day02/day02golf.py
+++ 2023/day02/day02golf.py
@@ -3,5 +3,5 @@ for i,l in enumerate(open("input.txt").readlines()):
  c=[0]*3
  for g in l.split(":")[1].split(";"):
   for e in g.split(","):n,x=e.split();x=len(x)-3;c[x]=max(c[x],int(n))
- a+=c[0]*c[1]*c[2];p.append((i+1)*all(c[x]<=(12,13,14)[x]for x in(0,1,2)))
+ a+=c[0]*c[1]*c[2];p.append((i+1)*all(c[x]<(13,14,15)[x]for x in(0,1,2)))
 print(sum(p),a)
