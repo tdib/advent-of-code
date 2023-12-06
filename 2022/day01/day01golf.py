8cd9525f9f3326ba2c84434a7ba45113a57bc602
tdib <github@tdib.xyz>
2023-12-06 17:52:47 +1100

    Properly golf day 1 2022

--- 2022/day01/day01golf.py
+++ 2022/day01/day01golf.py
@@ -1,1 +1,5 @@
-from itertools import groupby; print(sum(sorted([sum(x) for x in [list(g) for k, g in groupby([int(line.strip()) if line != '\n' else None for line in open('input.txt', 'r').readlines()], lambda x: x != None) if k]])[-3:]))
+S=[];m=s=0
+for l in open("input.txt").readlines():
+ if l=="\n":S+=[s];s=0
+ else:s+=int(l)
+S.sort();print(f"{' '.join([str(sum(S[-n:]))for n in[1,3]])}")
