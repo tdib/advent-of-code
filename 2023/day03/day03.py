f8dc2a10dcdcbe7c54064f0a0dfc8eeb374818ab
tdib <github@tdib.xyz>
2023-12-06 17:19:33 +1100

    Clean up day 3

--- 2023/day03/day03.py
+++ 2023/day03/day03.py
@@ -1,14 +1,19 @@
 # https://adventofcode.com/2023/day/3
-from collections import deque, defaultdict
-import itertools
 import re
+from math import prod
 
 with open("input.txt") as f:
     lines = list(map(str.strip, f.readlines()))
 
+
 def add_tuples(a, b):
   return (a[0] + b[0], a[1] + b[1])
 
+
+# Given two tuples like (1, 0), (5, 0), return a list of
+# tuples that fill in between these points
+# e.g. (1, 0), (5, 0) -> [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0)]
+# Note: This function assumes that start and end share an axis
 def fill(start, end):
     filled = set([start, end])
     # Row is the same - fill col
@@ -26,76 +31,32 @@ def fill(start, end):
 
     return tuple(filled)
 
-def print_map(lines, num_positions, symbol_positions, symbol_radii):
-    for row_idx, line in enumerate(lines):
-        for col_idx, char in enumerate(line):
-            pos = (row_idx, col_idx)
-            if False:
-                pass
-            elif pos in symbol_positions:
-                print(f"\033[91m{"x"}\033[0m", end="")
-                # print("X", end="")
-            elif pos in symbol_radii:
-                print(f"\033[91m{char}\033[0m", end="")
-                # print("o", end="")
-            elif pos in num_positions:
-                print(f"\033[94m{char}\033[0m", end="")
-                # print(char, end="")
-            else:
-                print(char, end="")
-        print()
 
 def solve_part_1():
-    symbol_coverage_positions = set()
-    new_symbols = set()
-    directions = [(0, 1), (0, -1), (-1, 0), (1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
     r = r'(\d+)'
     # Map positions to numbers
     position_num_map = {}
+    symbol_coverage_positions = set()
     for row_idx, line in enumerate(lines):
-        # matches = re.findall(r, line)
+        # Find integers and positions
         matches = re.finditer(r, line)
-        # num_matches = {m: 0 for m in matches}
         for match in matches:
-            # match_indexes = list(find_all(line, match))
-            # match_idx = match_indexes[num_matches[match]]
-            # num_matches[match] += 1
-            # match_start = match.start()
-            # print(match)
-            # print(match)
-            # print(match.start())
-            # print(match.end())
-            # print(match.group(0))
-            # match = match.group(1)
-            # else:
-                # match_idx = line.index(match)
-                # line = line[match_idx+1:]
-            # print(line, match, match_idx)
-            # print(list(find_all(line, match)))
-            # print()
-            # match_idx = find_nth_occurrence(line, match, match_num)
-            # print(x, match_idx)
-            # if len(matches) != len(set(matches)):
-            # match_idx = [i for i in range(len(line)) if line.startswith(match, i)][match_num]
-            if match.group(0) == "889":
-                print(match.start(), match.end()-1)
             filled = fill((row_idx, match.start()), (row_idx, match.end()-1))
             match = match.group(0)
             position_num_map[filled] = int(match)
-
-    for row_idx, line in enumerate(lines):
-        # Find symbols
+        
+        # Find symbol positions
         for col_idx, symbol_coverage_pos in enumerate(line):
             if symbol_coverage_pos != "." and not symbol_coverage_pos.isdigit():
                 symbol_coverage_positions.add((row_idx, col_idx))
             
     # Expand symbol radii
+    new_symbols = set()
+    directions = [(0, 1), (0, -1), (-1, 0), (1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
     for pos in symbol_coverage_positions:
         for dir in directions:
             new_symbols.add(add_tuples(pos, dir))
-    symbols = symbol_coverage_positions.copy()
-    symbol_coverage_positions = symbol_coverage_positions.union(new_symbols)
-
+    symbol_coverage_positions |= new_symbols
 
     adj = {}
     # Go through every number
@@ -104,153 +65,64 @@ def solve_part_1():
         for symbol_coverage_pos in symbol_coverage_positions:
             # If we have an intersection, we can say this value is adjacent
             if symbol_coverage_pos in curr_num_positions:
-                # if val == 239:
-                #     print(symbol_coverage_pos, curr_num_positions, symbol_coverage_pos in curr_num_positions, val)
-                #     pass
-                # adj[symbol_coverage_pos] = val
                 adj[curr_num_positions] = val
                 break
-    # print(adj)
 
-    # print_map(lines, position_num_map.keys())
-    flat_num_positions = [item for sublist in position_num_map.keys() for item in sublist]
-    flat_num_positions = [item for sublist in position_num_map.keys() for item in sublist]
-    # print_map(lines, flat_num_positions, symbols, symbol_coverage_positions)
-
-    # for k, v in position_num_map.items():
-    #     if v == 239:
-    #         print(k, v)
-    
-
-    # pos = position_num_map.values()
-    # print(set(position_num_map.values()) - set(adj.values()))
-    # print(position_num_map.values().adj.values()))
-    non_adj = []
-    for element in position_num_map.values():
-        if element not in adj.values():
-            non_adj.append(element)
-
-    adj = adj.values()
-    print(adj)
-
-    # print({ k:v for k, v in adj.items() if v == 889})
-    # print(list(filter(lambda x: x == 889, adj)))
-
-    return sum(adj)
-
-
+    return sum(adj.values())
 
 
 def solve_part_2():
-    symbol_coverage_positions = set()
-    new_symbols = set()
-    directions = [(0, 1), (0, -1), (-1, 0), (1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
+    ans = 0 
     r = r'(\d+)'
     # Map positions to numbers
     position_num_map = {}
+    symbol_coverage_positions = set()
     for row_idx, line in enumerate(lines):
+        # Find integers and positions
         matches = re.finditer(r, line)
         for match in matches:
             filled = fill((row_idx, match.start()), (row_idx, match.end()-1))
             match = match.group(0)
             position_num_map[filled] = int(match)
-
-    for row_idx, line in enumerate(lines):
-        # Find symbols
+        
+        # Find symbol positions
         for col_idx, symbol_coverage_pos in enumerate(line):
             if symbol_coverage_pos != "." and not symbol_coverage_pos.isdigit():
                 symbol_coverage_positions.add((row_idx, col_idx))
             
     # Expand symbol radii
+    new_symbols = set()
+    directions = [(0, 1), (0, -1), (-1, 0), (1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
     for pos in symbol_coverage_positions:
         for dir in directions:
             new_symbols.add(add_tuples(pos, dir))
-    symbols = symbol_coverage_positions.copy()
-    symbol_coverage_positions = symbol_coverage_positions.union(new_symbols)
+    symbol_coverage_positions |= new_symbols
 
-
-    # for line in lines:
-        # print(line)
-    ans = 0 
-    for symbol in symbols:
-        touching = []
-        radius_positions = set([symbol])
-        # If we find a gear
+    for symbol in symbol_coverage_positions:
+        # If this is not a gear, we don't care and can continue
         if lines[symbol[0]][symbol[1]] != "*":
             continue
-        print()
-        print("NOW LOOKING AT SYMBOL", symbol)
         
-        # Expand its radius
+        # Expand its radius (inefficient because we've already computed this but it's fine)
+        radius_positions = set([symbol])
         for dir in directions:
             radius_positions.add(add_tuples(symbol, dir))
 
-        
-        # Number of women i've touched
-        num_touches = 0
-        touches = {}
+        valid_gears = {}
         # Take each position in the radius
-        for r in radius_positions:
+        for radius_position in radius_positions:
             # Check if there is a number that exists in that radius
             for curr_num_positions, val in position_num_map.items():
-                found_touch = False
-                if r in curr_num_positions:
-                # for pos in curr_num_positions:
-                    # if pos in radius_positions:
-                    touches[curr_num_positions] = val
-                    touching.append(val)
-                    num_touches += 1
-                    found_touch = True
+                # The current number we are scanning has a digit within this radius
+                # so we can track this and move on to the next radius position
+                if radius_position in curr_num_positions:
+                    valid_gears[curr_num_positions] = val
                     break
-                
-        
-        print(symbol, num_touches)
-        touches = { k:v for k, v in touches.items() if len(touches) >= 2}
-        touches_thing = touches.values()
-        print(touches)
-        if touches_thing:
-            product = 1
-            for t in touches_thing:
-                product *= t
-            print(product)
-            ans += product
-                # If we have found one from the previous loop
-                # 
-                # if found_touch:
-                    
-            
-        # if symbol == "*":
 
-    adj = {}
-    # Go through every number
-    for curr_num_positions, val in position_num_map.items():
-        # Go through every place that a symbol covers
-        for symbol_coverage_pos in symbol_coverage_positions:
-            # If we have an intersection, we can say this value is adjacent
-            if symbol_coverage_pos in curr_num_positions:
-                # if val == 239:
-                #     print(symbol_coverage_pos, curr_num_positions, symbol_coverage_pos in curr_num_positions, val)
-                #     pass
-                # adj[symbol_coverage_pos] = val
-                adj[curr_num_positions] = val
-                break
-    # print(adj)
+        valid_gears = { k:v for k, v in valid_gears.items() if len(valid_gears) >= 2}
+        if valid_gears:
+            ans += prod(valid_gears.values())
 
-    flat_num_positions = [item for sublist in position_num_map.keys() for item in sublist]
-    # print_map(lines, flat_num_positions, symbols, symbol_coverage_positions)
-
-    non_adj = []
-    for element in position_num_map.values():
-        if element not in adj.values():
-            non_adj.append(element)
-
-    adj = adj.values()
-    print(adj)
-
-    # print({ k:v for k, v in adj.items() if v == 889})
-    # print(list(filter(lambda x: x == 889, adj)))
-
-    # 72553360 too high
     return ans
 
 
