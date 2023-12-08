45bd8993cc84316e52a54b2f378e09f693b35d90
tdib <github@tdib.xyz>
2023-12-08 15:44:42 +1100

    Clean up python implementation

--- 2023/day07/day07.py
+++ 2023/day07/day07.py
@@ -13,114 +13,54 @@ def sort_tuples(t, l):
     return sorted(t, key=lambda x: custom_sort_key(x, l))
 
 
-def solve_part_1():
-    labels = "A K Q J T 9 8 7 6 5 4 3 2".split()
-    ans = 0
+def classify_hand(hand, is_part_2):
+    counts = Counter(hand)
+    vals = counts.values()
+    j_count = counts.get("J", 0)
+    if is_part_2:
+        del counts["J"]
+    else:
+        j_count = 0
+    
+    if 5 - j_count in vals or j_count >= 4:
+        return 7
+    elif 4 - j_count in vals:
+        return 6
+    elif (len(list(filter(lambda x: x == 2, vals))) == 2 and j_count == 1) or (3 in vals and 2 in vals):
+        return 5
+    elif 3 - j_count in vals:
+        return 4
+    elif len(list(filter(lambda x: x == 2, vals))) == 2:
+        return 3
+    elif 2 - j_count in vals:
+        return 2
+    else:
+        return 1
+
+
+def solve(labels, is_part_2):
     hand_rankings = []
     for line in lines:
         hand, winnings = line.split()
-        winnings = int(winnings)
-
-        c = Counter(hand)
-        vals = c.values()
-
-        # Five of a kind
-        if 5 in vals:
-            hand_rankings.append((7, hand, winnings))
-        # Four of a kind
-        elif 4 in vals:
-            hand_rankings.append((6, hand, winnings))
-        # Full house
-        elif len(vals) == 2 and 2 in vals and 3 in vals:
-            hand_rankings.append((5, hand, winnings))
-        # Three of a kind
-        elif 3 in vals:
-            hand_rankings.append((4, hand, winnings))
-        # Two pair
-        elif len(vals) == 3 and len(list(filter(lambda x: x == 2, vals))) == 2:
-            hand_rankings.append((3, hand, winnings))
-        # One pair
-        elif len(vals) == 4 and 2 in vals:
-            hand_rankings.append((2, hand, winnings))
-        # High card
-        elif len(vals) == 5:
-            hand_rankings.append((1, hand, winnings))
-
+        hand_rankings.append((classify_hand(hand, is_part_2), hand, int(winnings)))
     # Sort them firstly according to their hand strength, and then by cards
     hand_rankings = sort_tuples(hand_rankings, labels)
+
+    ans = 0
     for i, (_, _, score) in enumerate(reversed(hand_rankings)):
         ans += score * (i+1)
 
     return ans
+
+
+def solve_part_1():
+    labels = "A K Q J T 9 8 7 6 5 4 3 2".split()
+    return solve(labels, is_part_2=False)
 
 
 def solve_part_2():
     labels = "A K Q T 9 8 7 6 5 4 3 2 J".split()
-    ans = 0
-    hand_rankings = []
-    for line in lines:
-        hand, winnings = line.split()
-        winnings = int(winnings)
-
-        c = Counter(hand)
-        vals = c.values()
-
-        j_count = 0
-        if "J" in c:
-            j_count = c["J"]
-            del c["J"]
-
-        # No J's - regular logic applies
-        if j_count == 0:
-            # Five of a kind
-            if 5 in vals:
-                hand_rankings.append((7, hand, winnings))
-            # Four of a kind
-            elif 4 in vals:
-                hand_rankings.append((6, hand, winnings))
-            # Full house
-            elif len(vals) == 2 and 2 in vals and 3 in vals:
-                hand_rankings.append((5, hand, winnings))
-            # Three of a kind
-            elif 3 in vals:
-                hand_rankings.append((4, hand, winnings))
-            # Two pair
-            elif len(vals) == 3 and len(list(filter(lambda x: x == 2, vals))) == 2:
-                hand_rankings.append((3, hand, winnings))
-            # One pair
-            elif len(vals) == 4 and 2 in vals:
-                hand_rankings.append((2, hand, winnings))
-            # High card
-            elif len(vals) == 5:
-                hand_rankings.append((1, hand, winnings))
-        # At least one J - updated logic applies
-        else:
-            # Five of a kind
-            if any(x + j_count == 5 for x in vals) or j_count in [4, 5]:
-                hand_rankings.append((7, hand, winnings))
-            # Four of a kind
-            elif any(x + j_count == 4 for x in vals):
-                hand_rankings.append((6, hand, winnings))
-            # Full house
-            elif len(vals) == 2 and j_count == 1:
-                hand_rankings.append((5, hand, winnings))
-            # Three of a kind
-            elif any(x + j_count == 3 for x in vals):
-                hand_rankings.append((4, hand, winnings))
-            # Two pair
-            elif len(vals) == 3 and len(list(filter(lambda x: x == 2, c.values()))) == 2:
-                hand_rankings.append((3, hand, winnings))
-            # One pair
-            elif len(vals) == 4 and (any(x == 2 for x in c.values()) or j_count == 1):
-                hand_rankings.append((2, hand, winnings))
-            # High card
-            elif all([x == 1 for x in vals]):
-                hand_rankings.append((1, hand, winnings))
-
-    hand_rankings = sort_tuples(hand_rankings, labels)
-    for i, (_, _, score) in enumerate(reversed(hand_rankings)):
-        ans += score * (i+1)
-    return ans
+    return solve(labels, is_part_2=True)
 
 
 print(f"Part 1 answer: {solve_part_1()}")
