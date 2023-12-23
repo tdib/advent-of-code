79beb939a83e8b478a67b2a862833e3824b49a68
tdib <github@tdib.xyz>
2023-12-23 23:51:41 +1100

    Add util file

---
+++ util/util.py
@@ -0,0 +1,250 @@
+from typing import Callable
+from collections import deque
+
+class C:
+    RED = "\033[91m"
+    ORANGE = "\033[38;5;208m"
+    YELLOW = "\033[38;5;220m"
+    GREEN = "\033[92m"
+    CYAN = "\033[36m"
+    BLUE = "\033[94m"
+    PINK = "\033[38;5;207m"
+    PURPLE = "\033[38;5;93m"
+
+    BOLD = "\033[1m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+
+    ENDC = "\033[0m"
+
+# Alias print to d() to allow for quicker typing :^)
+d = print
+
+UP = (-1, 0)
+DOWN = (1, 0)
+LEFT = (0, -1)
+RIGHT = (0, 1)
+DIRECTIONS = [UP, DOWN, LEFT, RIGHT]
+
+dir_to_str = { UP: "up", DOWN: "down", LEFT: "left", RIGHT: "right" }
+
+dir_to_symbol = { UP: "^", DOWN: "v", LEFT: "<", RIGHT: ">" }
+symbol_to_dir = { v: k for k, v in dir_to_symbol.items() }
+
+WALL = "#"
+EMPTY = "."
+
+def limit_vector(vector: tuple[int], magnitude: int, use_abs_value: bool = False):
+    """
+    Given an n-dimensional vector, limit the magnitude of its components. For example, given
+    (1, 6, 3) with magnitude 5, the output would be (1, 5, 3).
+
+    By default, the function ignores negatives, so (1, -6, 3) with magnitude 5 would output (1, -6, 3).
+    If `use_abs_value` is set to true, the output would instead be (1, -5, 3).
+    """
+    if use_abs_value:
+        return tuple(-1 * min(abs(v), magnitude) if v < 0 else min(abs(v), magnitude) for v in vector)
+    else:
+        return tuple(min(v, magnitude) for v in vector)
+
+def add_vectors(v1: tuple[int], v2: tuple[int]) -> tuple[int]:
+    """
+    Add two n-dimensional vectors.
+
+    ```
+    v1 = (1, 2, 3)
+    v2 = (0, 6, -2)
+    assert add_vectors(v1, v2) == (1, 8, 1)
+    ```
+    """
+    return tuple(elem1 + elem2 for elem1, elem2 in zip(v1, v2))
+
+def subtract_vectors(v1: tuple[int], v2: tuple[int]) -> tuple[int]:
+    """
+    Subtract two n-dimensional vectors.
+
+    ```
+    v1 = (1, 2, 3)
+    v2 = (0, 6, -2)
+    assert subtract_vectors(v1, v2) == (1, -4, 5)
+    ```
+    """
+    return tuple(elem1 - elem2 for elem1, elem2 in zip(v1, v2))
+
+def multiply_vectors(v1: tuple[int], v2: tuple[int]) -> tuple[int]:
+    """
+    Multiply two n-dimensional vectors.
+
+    ```
+    v1 = (1, 2, 3)
+    v2 = (0, 6, -2)
+    assert multiply_vectors(v1, v2) == (0, 12, -6)
+    ```
+    """
+    return tuple(elem1 * elem2 for elem1, elem2 in zip(v1, v2))
+
+def fill_volume_3d(pos1: tuple[int], pos2: tuple[int]) -> set[tuple[int]]:
+    """
+    Given two positions in 3d space, fills all positions that exist between the two points.
+    For example, given (0, 0, 0), and (0, 1, 2), the function returns
+    {(0, 0, 0), (0, 0, 1), (0, 0, 2), (0, 1, 0), (0, 1, 1), (0, 1, 2)}
+    """
+    x1, y1, z1 = pos1
+    x2, y2, z2 = pos2
+    filled = set()
+
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            for z in range(z1, z2 + 1):
+                filled.add((x, y, z))
+    
+    return filled
+
+def fill_volume_2d(pos1: tuple[int], pos2: tuple[int]) -> set[tuple[int]]:
+    """
+    Given two positions in 2d space, fills all positions that exist between the two points.
+    For example, given (0, 0), and (2, 2), the function returns
+    {(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)}
+    """
+    x1, y1 = pos1
+    x2, y2 = pos2
+    filled = set()
+
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            filled.add((x, y))
+    
+    return filled
+
+def search(map: list[str], start: tuple[int], position_predicate: Callable[[tuple[int]], bool] = lambda x: x, target: tuple[int] = None, mode: str = "bfs"):
+    """
+    Given a list of strings representing the rows in a map, performs a search from the start position.
+
+    If a position predicate is not provided, the search simply search traverses the entire map (disregarding any "walls").
+
+    If a target is not provided, the search will be exhaustive.
+
+    If a mode is not provided, the search will default to BFS. Options for mode are "bfs" and "dfs"
+
+    Returns:
+    A set of visited positions, and the distances to each of those nodes
+    """
+    queue = deque([start])
+    visited = set()
+    dists = { start: 0 }
+    curr_pos = start
+    while queue:
+        if mode == "bfs":
+            curr_pos = queue.popleft()
+        elif mode == "dfs":
+            curr_pos = queue.pop()
+        else:
+            raise RuntimeError(f"Mode {mode} was not recognised as a valid search type.")
+
+        visited.add(curr_pos)
+
+        if curr_pos == target:
+            return visited, dists
+
+        for direction in DIRECTIONS:
+            next_pos = add_vectors(curr_pos, direction)
+            if next_pos not in visited and within_bounds(next_pos, map) and position_predicate(next_pos):
+                queue.append(next_pos)
+                dists[next_pos] = dists[curr_pos] + 1
+        
+    return visited, dists
+
+
+# Simply checks if the given position falls within the bounds of the provided array
+def within_bounds(pos: tuple[int], arr: list[str]) -> bool:
+    """
+    Checks if the given position falls within the bounds of the provided array.
+
+    Returns True if the position is within the bounds, False otherwise
+    """
+    row, col = pos
+    return row >= 0 and row < len(arr) and col >= 0 and col < len(arr[0])
+
+def read_grid_positions(grid: list[str], chars: list[str]) -> list[set[tuple]]:
+    """
+    Given a list of strings representing a grid, and a list of chars to search for, returns a
+    list of positions of the given characters.
+
+    Note: The returned list will correspond to the chars list provided
+
+    ```
+    grid = [
+     [..X.],
+     [XX..],
+     [.S..],
+    ]
+    chars = ["S", "X"]
+
+    assert read_grid_positions(grid, chars) == [{(0, 2), (1, 0), (1, 1)}, {(2, 1)}]
+    ```
+    """
+    positions = [set() for _ in range(len(chars))]
+    for row_idx, row in enumerate(grid):
+        for col_idx, ch in enumerate(row):
+            if ch in chars:
+                positions[chars.index(ch)].add((row_idx, col_idx))
+    return positions
+
+class Notable:
+    """
+    Defines a "notable" element on a map. For example, given the positions of all "walls" in a map,
+    you can provide the positions to this class, and define how they will be displayed when visualising
+    the map.
+
+    For example, you may wish to print your visited nodes using a different symbol, or in a different colour.
+    """
+    def __init__(self, positions: set[tuple[int]], symbol: str = "X", colour: str = C.RED):
+        self.positions = positions
+        self.symbol = symbol
+        self.colour = colour
+
+def print_map(grid: list[str], notables: list[Notable] = []):
+    """
+    Print a visualisation of the provided grid. A list of notables may be passed in to display
+    different elements in a different colour.
+
+    Note: The notables passed in should contain highest -> lowest precedence, as the first match
+    to a position will be printed, and the others ignored. This is only relevant if you have the same position
+    in multiple sets (e.g. all_positions and visited_positions).
+    """
+    for row_idx, row in enumerate(grid):
+        for col_idx, ch in enumerate(row):
+            curr = (row_idx, col_idx)
+            for notable in notables:
+                if curr in notable.positions:
+                    print(f"{notable.colour}{notable.symbol}{C.ENDC}", end="")
+                    break
+            else:
+                print(ch, end="")
+        print()
+    print()
+
+def read_as_chunks(file_name):
+    """
+    Given a file that contains portions/chunks separated by newlines, read the input
+    into various chunks.
+    """
+    with open(file_name) as f:
+        chunks = list(map(str.splitlines, f.read().split("\n\n")))
+    return chunks
+    
+def read_as_lines(file_name):
+    """
+    Given a file that contains numerous lines of the same format, read each line into its own string in a list
+    """
+    with open(file_name) as f:
+        lines = list(map(str.strip, f.readlines()))
+    return lines
+
+def read_as_single_line(file_name):
+    """
+    Given a single line file, read the single line into a string
+    """
+    with open(file_name) as f:
+        line = f.readline()
+    return line
