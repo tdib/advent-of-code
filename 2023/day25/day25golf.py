a27bcdc97683ca980827f9d7b89ddfe75b455736
tdib <github@tdib.xyz>
2023-12-26 14:33:26 +1100

    Rewrite golf (222 -> 212)

--- 2023/day25/day25golf.py
+++ 2023/day25/day25golf.py
@@ -1,1 +1,5 @@
-from networkx import*;g=Graph();[g.add_edge(l.split(":")[0],c)for l in open("input.txt").readlines()for c in l.split(":")[1].split()];g.remove_edges_from(minimum_edge_cut(g));a,b=map(len,connected_components(g));print(a*b)
+from networkx import*;g=Graph()
+for l in open("input.txt").readlines():n,c=l.split(":");[g.add_edge(n,q)for q in c.split()]
+g.remove_edges_from(minimum_edge_cut(g))
+a,b=map(len,connected_components(g))
+print(a*b)
