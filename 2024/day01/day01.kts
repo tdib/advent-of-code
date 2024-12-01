6b783d01e95cfe524c501abcc2237b8a3fb31d21
Dib <79500311+tdib@users.noreply.github.com>
2024-12-01 16:25:32 +1100

    Slightly clean up day 1

--- 2024/day01/day01.kts
+++ 2024/day01/day01.kts
@@ -4,24 +4,21 @@ import kotlin.math.abs
 val lines = File("input.txt").readLines()
 
 fun solvePart1(): Int {
-    var ret = 0
     val r = Regex("\\d+")
     val listA = mutableListOf<Int>()
     val listB = mutableListOf<Int>()
+
     for (line in lines) {
-        val nums = r.findAll(line).map { it.value }.toList()
-        listA.add(nums.first().toInt())
-        listB.add(nums.last().toInt())
+        r.findAll(line).map { it.value.toInt() }.apply {
+            listA.add(this.first())
+            listB.add(this.last())
+        }
     }
 
     listA.sort()
     listB.sort()
 
-    for ((a, b) in listA.zip(listB)) {
-        ret += abs(a - b)
-    }
-
-    return ret
+    return listA.zip(listB).sumOf { abs(it.first - it.second) }
 }
 
 fun solvePart2(): Int {
@@ -29,6 +26,7 @@ fun solvePart2(): Int {
     val r = Regex("\\d+")
     val listA = mutableListOf<Int>()
     val listB = mutableListOf<Int>()
+
     for (line in lines) {
         val nums = r.findAll(line).map { it.value }.toList()
         listA.add(nums.first().toInt())
@@ -36,7 +34,6 @@ fun solvePart2(): Int {
     }
 
     val counts = listB.groupingBy { it }.eachCount()
-
     for (item in listA) {
         ret += item * counts.getOrDefault(item, 0)
     }
