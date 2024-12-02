d1fcebbd3372ab2366b4c6e62123e27e8cd367b1
Dib <79500311+tdib@users.noreply.github.com>
2024-12-02 22:06:48 +1100

    Clean up day 2

--- 2024/day02/day02.kts
+++ 2024/day02/day02.kts
@@ -27,32 +27,23 @@ fun isSafe(nums: List<Int>, threshold: Int = 3) =
 
 fun Boolean.toInt() = if (this) 1 else 0
 
-fun solvePart1(): Int {
-    val r = Regex("\\d+")
-    var total = 0
-    for (line in lines) {
+val r = Regex("\\d+")
+fun solvePart1() =
+    lines.sumOf { line ->
         val nums = r.findAll(line).map { it.value.toInt() }.toList()
-        if (getDirection(nums) != Direction.NONE) {
-            total += isSafe(nums).toInt()
-        }
+        (getDirection(nums) != Direction.NONE && isSafe(nums)).toInt()
     }
-    return total
-}
 
-fun solvePart2(): Int {
-    val r = Regex("\\d+")
-    var total = 0
-    for (line in lines) {
+fun solvePart2() =
+    lines.sumOf { line ->
         val nums = r.findAll(line).map { it.value.toInt() }.toList()
         val numsRemoved = listOf(nums) + List(nums.size) { index ->
             nums.filterIndexed { i, _ -> i != index }
         }
-        total += numsRemoved.any { trial ->
+        numsRemoved.any { trial ->
             getDirection(trial) != Direction.NONE && isSafe(trial)
         }.toInt()
     }
-    return total
-}
 
 println("Part 1 answer: ${solvePart1()}")
 println("Part 2 answer: ${solvePart2()}")
