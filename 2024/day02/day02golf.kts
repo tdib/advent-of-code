d6591844acb08729260b63b259995854e689b621
Dib <79500311+tdib@users.noreply.github.com>
2024-12-03 10:59:27 +1100

    Optimise day 2 golf

--- 2024/day02/day02golf.kts
+++ 2024/day02/day02golf.kts
@@ -1,4 +1,4 @@
 fun f(n:List<Int>):Boolean{val d=n.zipWithNext().map{(a,b)->a-b}
 return d.all{it in 1..3}||d.all{it in-3..-1}}
 val n=java.io.File("input.txt").readLines().map{it.split(" ").map{it.toInt()}}
-print("Part 1 answer: ${n.count(::f)}\nPart 2 answer: ${n.count{List(it.size){j->it.filterIndexed{i,_->i!= j}}.any{f(it)}}}")
+print("Part 1 answer: ${n.count(::f)}\nPart 2 answer: ${n.count{List(it.size){j->it.filterIndexed{i,_->i!= j}}.any(::f)}}")
