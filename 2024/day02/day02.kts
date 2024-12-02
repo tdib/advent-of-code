575d3c79ebab43b08841044e465d765e51a9766a
Dib <79500311+tdib@users.noreply.github.com>
2024-12-02 22:30:30 +1100

    Clean up day 2

--- 2024/day02/day02.kts
+++ 2024/day02/day02.kts
@@ -25,24 +25,21 @@ fun getDirection(nums: List<Int>): Direction {
 fun isSafe(nums: List<Int>, threshold: Int = 3) =
     nums.zipWithNext().any { (prev, curr) -> abs(prev - curr) > threshold }.not()
 
-fun Boolean.toInt() = if (this) 1 else 0
-
-val r = Regex("\\d+")
 fun solvePart1() =
-    lines.sumOf { line ->
-        val nums = r.findAll(line).map { it.value.toInt() }.toList()
-        (getDirection(nums) != Direction.NONE && isSafe(nums)).toInt()
+    lines.count { line ->
+        val nums = line.split(" ").map { it.toInt() }.toList()
+        getDirection(nums) != Direction.NONE && isSafe(nums)
     }
 
 fun solvePart2() =
-    lines.sumOf { line ->
-        val nums = r.findAll(line).map { it.value.toInt() }.toList()
+    lines.count { line ->
+        val nums = line.split(" ").map { it.toInt() }.toList()
         val numsRemoved = listOf(nums) + List(nums.size) { index ->
             nums.filterIndexed { i, _ -> i != index }
         }
         numsRemoved.any { trial ->
             getDirection(trial) != Direction.NONE && isSafe(trial)
-        }.toInt()
+        }
     }
 
 println("Part 1 answer: ${solvePart1()}")
