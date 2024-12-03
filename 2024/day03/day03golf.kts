3e36e8a1ef6b93000b19082dc7cc1b8e6709847a
Dib <79500311+tdib@users.noreply.github.com>
2024-12-03 20:27:21 +1100

    Improve day 3 golf (483 -> 414)

--- 2024/day03/day03golf.kts
+++ 2024/day03/day03golf.kts
@@ -1,6 +1,6 @@
 val l=java.io.File("input.txt").readLines()
-enum class I(val r:String){M("mul\\((\\d+),(\\d+)\\)"),Y("do\\(\\)"),D("don't\\(\\)")}var m=0
-print("Part 1 answer: ${l.sumOf{Regex(I.M.r).findAll(it).map{it.groupValues.drop(1).map{it.toInt()}}.map{it[1]*it[2]}.sum()}}\nPart 2 answer: ${l.sumOf{Regex(I.entries.joinToString("|"){it.r}).findAll(it).map{it.groupValues}.map{g->when(I.entries.find{g[0].matches(it.r.toRegex())}){I.Y->m=1
-I.D->m=0 else->{}}
-g[1].toInt()*g[2].toInt()*m}
-.sum()}}")
+val r="""mul\((\d+),(\d+)\)|do\(\)|don't\(\)"""
+var m=0
+print("Part 1 answer: ${l.sumOf{Regex(r.take(18)).findAll(it).map{it.groupValues.drop(1).map{it.toInt()}}.map{it[0]*it[1]}.sum()}}\n Part 2 answer: ${l.sumOf{Regex(r).findAll(it).map{it.groupValues}.map{g->var r=0
+if(g[0].contains("o("))m=1 else if(g[0].contains("n"))m=0 else r=g[1].toInt()*g[2].toInt()*m
+r}.sum()}}")
