250bf15ce553589c37cbdef6cee6ded0dcb51168
Dib <79500311+tdib@users.noreply.github.com>
2024-12-03 20:22:41 +1100

    Drastically optimise day 3 minimal

--- 2024/day03/day03minimal.kts
+++ 2024/day03/day03minimal.kts
@@ -2,38 +2,30 @@ import java.io.File
 
 val l = File("input.txt").readLines()
 
-enum class I(val r: String) {
-    M("mul\\((\\d+),(\\d+)\\)"),
-    Y("do\\(\\)"),
-    D("don't\\(\\)"),
-}
+val r = """mul\((\d+),(\d+)\)|do\(\)|don't\(\)"""
 
 fun solvePart1() =
     l.sumOf {
-        Regex(I.M.r).findAll(it)
+        Regex(r.take(18)).findAll(it)
             .map { it.groupValues.drop(1).map { it.toInt() } }
             .map { it[0] * it[1] }
             .sum()
     }
 
-fun solvePart2(): Int {
-    var m = 0
-    return l.sumOf {
-        Regex(I.entries.joinToString("|") { it.r }).findAll(it)
+var m = 0
+fun solvePart2() =
+    l.sumOf {
+        Regex(r).findAll(it)
             .map { it.groupValues }
             .map { g ->
-                var r = 0
-                when (I.entries.find { g[0].matches(it.r.toRegex()) }) {
-                    I.Y -> m = 1
-                    I.D -> m = 0
-                    I.M -> r = g[1].toInt() * g[2].toInt() * m
-                    else -> {}
-                }
+                var r=0
+                if (g[0].contains("o(")) m = 1
+                else if (g[0].contains("n")) m = 0
+                else r = g[1].toInt() * g[2].toInt() * m
                 r
             }
             .sum()
     }
-}
 
 println("Part 1 answer: ${solvePart1()}")
 println("Part 2 answer: ${solvePart2()}")
