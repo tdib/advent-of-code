f7bf276e6f4c5b89b690fe3fb784d6d6b3c924c1
Dib <79500311+tdib@users.noreply.github.com>
2024-12-03 11:13:10 +1100

    Optimise minimal day 2

--- 2024/day02/day02minimal.kts
+++ 2024/day02/day02minimal.kts
@@ -11,7 +11,7 @@ fun solvePart1() =
     n.count(::f)
 
 fun solvePart2() =
-    n.count { List(it.size) { j -> it.filterIndexed { i, _ -> i != j } }.any(::f) }
+    n.count { it.indices.map { j -> it.take(j) + it.drop(j+1) }.any(::f) }
 
 println("Part 1 answer: ${solvePart1()}")
 println("Part 2 answer: ${solvePart2()}")
