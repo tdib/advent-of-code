8bc2d7a11f2313fdaaf73a12c5a87925eb94f8ae
Dib <79500311+tdib@users.noreply.github.com>
2024-12-04 20:28:34 +1100

    Clean up day 4

--- 2024/day04/day04.kts
+++ 2024/day04/day04.kts
@@ -14,11 +14,16 @@ fun rotate90(list: List<String>) =
         list.map { it[i] }.joinToString("").reversed()
     }
 
+/**
+ * Rotate an entire matrix 45 degrees, n times
+ */
 fun rotate45(list: List<String>, times: Int): List<String> {
     var retVal: List<String> = list
+    // How many quarter turns
     for (rotation in 1..times.floorDiv(2)) {
         retVal = rotate90(retVal)
     }
+    // Add dots to emulate a 45 degree turn, but only we require it (`times` must be divisible by 2)
     if (times % 2 != 0) {
         retVal = retVal.mapIndexed { i, row ->
             List(row.length - i - 1) { "." }.joinToString("") +
@@ -47,7 +52,6 @@ fun solvePart1(): Int {
 }
 
 fun solvePart2(): Int {
-    val r = Regex("MAS")
     var total = 0
     val windowHeight = 3
     val windowWidth = 3
@@ -59,28 +63,15 @@ fun solvePart2(): Int {
             val subArray = lines.subList(row, row + windowHeight).map { str ->
                 str.substring(col, col + windowWidth)
             }
-            if (subArray[1][1] == 'A') {
-                val topLeft = subArray.first().first()
-                val topRight = subArray.first().last()
-                val bottomLeft = subArray.last().first()
-                val bottomRight = subArray.last().last()
-                val sanitisedList = listOf(
-                    "$topLeft.$topRight",
-                    ".A.",
-                    "$bottomLeft.$bottomRight"
-                )
-                var m: List<String>
-                var tempTotal = 0
-                for (i in 0..7) {
-                    m = rotate45(sanitisedList, i)
-                    for (l in m) {
-                        val matchCount = r.findAll(l).count()
-                        tempTotal += matchCount
-                    }
-                }
-                if (tempTotal == 2) {
-                    total++
-                }
+            val topLeft = subArray.first().first()
+            val topRight = subArray.first().last()
+            val bottomLeft = subArray.last().first()
+            val bottomRight = subArray.last().last()
+            val chars = listOf(topLeft, topRight, bottomRight, bottomLeft).joinToString("")
+            // We only care about the corner characters, and they must form one of the following, or it is not an X-MAS
+            val validCorners = listOf("MMSS", "MSSM", "SSMM", "SMMS")
+            if (subArray[1][1] == 'A' && chars in validCorners) {
+                total++
             }
         }
     }
