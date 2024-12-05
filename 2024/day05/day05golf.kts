f17665c72cc0e1f8a543d79b1378779e5587ca49
Dib <79500311+tdib@users.noreply.github.com>
2024-12-05 21:55:16 +1100

    Improve day 5 golf

--- 2024/day05/day05golf.kts
+++ 2024/day05/day05golf.kts
@@ -4,6 +4,6 @@ val u=j.lineSequence().map{line->line.split(",").map{it.toInt()}}
 var x=0
 var y=0
 fun v(n:List<Int>)=n.fold(listOf<Int>()){a,b->if(a.intersect(d[b].orEmpty()).isEmpty())a+b else return false}.let{true}
-u.forEach{if(v(it)){x+=it[it.size.floorDiv(2)]}else{y+=it.sortedWith{a,b->when{b in d[a].orEmpty() -> -1
-a in d[b].orEmpty() -> 1 else -> 0}}[it.size.floorDiv(2)]}}
+u.forEach{if(v(it)){x+=it[it.size.floorDiv(2)]}else{y+=it.sortedWith{a,b->when{b in d[a].orEmpty()->-1
+a in d[b].orEmpty()->1 else->0}}[it.size.floorDiv(2)]}}
 print("Part 1 answer: $x\nPart 2 answer: $y")
