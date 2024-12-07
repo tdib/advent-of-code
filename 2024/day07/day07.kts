2abdc4b2d4764538fb36428761ddd5ebf6d73426
Dib <79500311+tdib@users.noreply.github.com>
2024-12-07 17:53:28 +1100

    Implement Stefan's recursive solution :)

--- 2024/day07/day07.kts
+++ 2024/day07/day07.kts
@@ -2,65 +2,35 @@ import java.io.File
 
 val lines = File("input.txt").readLines()
 
-enum class Operation {
-    MULTIPLY,
-    ADD,
-    OR;
+// Thank you, Stefan :)
+fun solve(values: List<Long>, desiredResult: Long, part: Int, currValue: Long = 0L): Boolean {
+    if (currValue == desiredResult && values.isEmpty()) {
+        return true
+    } else if (currValue > desiredResult || values.isEmpty()) {
+        return false
+    }
+    val valuesMutable = values.toMutableList()
+    val nextNum = valuesMutable.removeFirst()
 
-    companion object {
-        fun forPart1() = listOf(MULTIPLY, ADD)
-        fun forPart2() = listOf(MULTIPLY, ADD, OR)
+    val resultPlus = currValue + nextNum
+    val resultMult = currValue * nextNum
+    val resultOr = if (part == 2) (currValue.toString() + nextNum).toLong() else null
+    return listOfNotNull(resultPlus, resultMult, resultOr).any { currResult ->
+        solve(valuesMutable, desiredResult, part, currResult)
     }
 }
 
-fun generatePermutations(n: Int, values: List<Operation>): List<List<Operation>> {
-    if (n == 1) return listOf(emptyList())
-    val smallerPermutations = generatePermutations(n - 1, values)
-    return smallerPermutations.flatMap { perm -> values.map { value -> perm + value } }
-}
-
-fun solve(part: Int): Long {
-    var total = 0L
-    for (line in lines) {
+fun solvePart(part: Int) =
+    lines.sumOf { line ->
         val split = line.split(": ")
-        val result = split.first().toLong()
+        val desiredResult = split.first().toLong()
         val nums = split.last().split(" ").map { it.toLong() }
-        val permittedOperations = when (part) {
-            1 -> Operation.forPart1()
-            2 -> Operation.forPart2()
-            else -> error("Invalid part number provided")
-        }
-        val opPerms = generatePermutations(nums.size, permittedOperations)
-
-        for (opPerm in opPerms) {
-            var currSum = nums.first()
-            for ((num, op) in nums.drop(1).zip(opPerm)) {
-                when (op) {
-                    Operation.MULTIPLY -> currSum *= num
-                    Operation.ADD -> currSum += num
-                    Operation.OR -> if (part == 1) {
-                        error("Invalid part 1 operation")
-                    } else {
-                        currSum = (currSum.toString() + num).toLong()
-                    }
-                }
-                if (currSum > result) {
-                    break
-                }
-            }
-            if (currSum == result) {
-                total += result
-                break
-            }
-        }
+        if (solve(nums, desiredResult, part = part)) desiredResult else 0
     }
-    return total
 
-}
+fun solvePart1() = solvePart(1)
 
-fun solvePart1() = solve(part = 1)
-
-fun solvePart2() = solve(part = 2)
+fun solvePart2() = solvePart(2)
 
 println("Part 1 answer: ${solvePart1()}")
 println("Part 2 answer: ${solvePart2()}")
