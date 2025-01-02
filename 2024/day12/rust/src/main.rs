a5c36488b16eb67dc0708b026d8f03c747469677
Dib <79500311+tdib@users.noreply.github.com>
2025-01-02 17:40:21 +1100

    Clean up some repeated code

--- 2024/day12/rust/src/main.rs
+++ 2024/day12/rust/src/main.rs
@@ -129,182 +129,87 @@ impl Region {
         self.positions.len()
     }
 
-    fn get_perimeter(&self) -> usize {
-        fn count_left_to_right(
-            positions: &HashSet<Position>,
-            min_row: isize,
-            min_col: isize,
-            max_row: isize,
-            max_col: isize,
-        ) -> usize {
-            let left_start = Position {
-                row: min_row,
-                col: min_col,
-            }
-            .offset(&LEFT);
+    fn get_perimeter(&self, part: usize) -> usize {
+        let mut perimeter = 0;
+        let mut curr_pos;
+        let mut perimeter_locs;
 
-            let mut perimeter = 0;
-            let mut curr_pos = left_start;
-
-            // Scan left to right, counting boundaries on the left and right sides
-            while curr_pos.row <= max_row {
-                let mut prev = false;
-                let mut curr;
-                // Scan right until we have passed the rightmost column position
-                while curr_pos.col <= max_col + 1 {
-                    curr = positions.contains(&curr_pos);
-
-                    if prev != curr {
-                        perimeter += 1;
-                        prev = curr;
-                    }
-
-                    curr_pos = curr_pos.offset(&RIGHT);
-                }
-                curr_pos = curr_pos.offset(&DOWN);
-                curr_pos.col = left_start.col;
-            }
-
-            perimeter
-        }
-
-        fn count_top_to_bottom(
-            positions: &HashSet<Position>,
-            min_row: isize,
-            min_col: isize,
-            max_row: isize,
-            max_col: isize,
-        ) -> usize {
-            let top_start = Position {
-                row: min_row,
-                col: min_col,
-            }
-            .offset(&UP);
-
-            let mut perimeter = 0;
-            let mut curr_pos = top_start;
-
-            // Scan top to bottom, counting boundaries on the top and bottom sides
-            while curr_pos.col <= max_col {
-                let mut prev = false;
-                let mut curr;
-                // Scan right until we have passed the rightmost column position
-                while curr_pos.row <= max_row + 1 {
-                    curr = positions.contains(&curr_pos);
-
-                    if prev != curr {
-                        perimeter += 1;
-                        prev = curr;
-                    }
-
-                    curr_pos = curr_pos.offset(&DOWN);
-                }
-                curr_pos = curr_pos.offset(&RIGHT);
-                curr_pos.row = top_start.row;
-            }
-
-            perimeter
-        }
         let min_row = self.positions.iter().min_by_key(|pos| pos.row).unwrap().row;
         let max_row = self.positions.iter().max_by_key(|pos| pos.row).unwrap().row;
         let min_col = self.positions.iter().min_by_key(|pos| pos.col).unwrap().col;
         let max_col = self.positions.iter().max_by_key(|pos| pos.col).unwrap().col;
 
-        count_left_to_right(&self.positions, min_row, min_col, max_row, max_col)
-            + count_top_to_bottom(&self.positions, min_row, min_col, max_row, max_col)
-    }
-
-    fn get_perimeter2(&self) -> usize {
-        fn count_left_to_right(
-            positions: &HashSet<Position>,
-            min_row: isize,
-            min_col: isize,
-            max_row: isize,
-            max_col: isize,
-        ) -> usize {
-            let left_start = Position {
-                row: min_row,
-                col: min_col,
-            }
-            .offset(&LEFT);
-
-            let mut perimeter = 0;
-            let mut perimeter_locs: HashSet<(Position, bool)> = HashSet::new();
-            let mut curr_pos = left_start;
-
-            // Scan left to right, counting boundaries on the left and right sides
-            while curr_pos.row <= max_row {
-                let mut prev = false;
-                let mut curr;
-                // Scan right until we have passed the rightmost column position
-                while curr_pos.col <= max_col + 1 {
-                    curr = positions.contains(&curr_pos);
-
-                    if prev != curr {
-                        if !perimeter_locs.contains(&(curr_pos.offset(&UP), curr)) {
-                            perimeter += 1;
-                        }
-                        perimeter_locs.insert((curr_pos, curr));
-                        prev = curr;
-                    }
-
-                    curr_pos = curr_pos.offset(&RIGHT);
-                }
-                curr_pos = curr_pos.offset(&DOWN);
-                curr_pos.col = left_start.col;
-            }
-
-            perimeter
+        // Raster scan (left -> right for each row), counting all boundaries on left and right sides
+        let left_start = Position {
+            row: min_row,
+            col: min_col,
         }
+        .offset(&LEFT);
+        curr_pos = left_start;
+        perimeter_locs = HashSet::new();
+        while curr_pos.row <= max_row {
+            let mut prev = false;
+            // let mut curr;
+            // Scan right until we have passed the rightmost column position
+            while curr_pos.col <= max_col + 1 {
+                let curr = self.positions.contains(&curr_pos);
 
-        fn count_top_to_bottom(
-            positions: &HashSet<Position>,
-            min_row: isize,
-            min_col: isize,
-            max_row: isize,
-            max_col: isize,
-        ) -> usize {
-            let top_start = Position {
-                row: min_row,
-                col: min_col,
-            }
-            .offset(&UP);
-
-            let mut perimeter = 0;
-            let mut curr_pos = top_start;
-            let mut perimeter_locs: HashSet<(Position, bool)> = HashSet::new();
-
-            // Scan top to bottom, counting boundaries on the top and bottom sides
-            while curr_pos.col <= max_col {
-                let mut prev = false;
-                let mut curr;
-                // Scan right until we have passed the rightmost column position
-                while curr_pos.row <= max_row + 1 {
-                    curr = positions.contains(&curr_pos);
-
-                    if prev != curr {
-                        if !perimeter_locs.contains(&(curr_pos.offset(&LEFT), curr)) {
-                            perimeter += 1;
+                if prev != curr {
+                    match part {
+                        1 => perimeter += 1,
+                        2 => {
+                            if !perimeter_locs.contains(&(curr_pos.offset(&UP), curr)) {
+                                perimeter += 1;
+                            }
+                            perimeter_locs.insert((curr_pos, curr));
                         }
-                        perimeter_locs.insert((curr_pos, curr));
-                        prev = curr;
+                        _ => unreachable!(),
                     }
-
-                    curr_pos = curr_pos.offset(&DOWN);
+                    prev = curr;
                 }
+
                 curr_pos = curr_pos.offset(&RIGHT);
-                curr_pos.row = top_start.row;
             }
-
-            perimeter
+            curr_pos = curr_pos.offset(&DOWN);
+            curr_pos.col = left_start.col;
         }
-        let min_row = self.positions.iter().min_by_key(|pos| pos.row).unwrap().row;
-        let max_row = self.positions.iter().max_by_key(|pos| pos.row).unwrap().row;
-        let min_col = self.positions.iter().min_by_key(|pos| pos.col).unwrap().col;
-        let max_col = self.positions.iter().max_by_key(|pos| pos.col).unwrap().col;
 
-        count_left_to_right(&self.positions, min_row, min_col, max_row, max_col)
-            + count_top_to_bottom(&self.positions, min_row, min_col, max_row, max_col)
+        // Vertical raster scan (top -> bottom for each col), counting all boundaries on top and bottom sides
+        let top_start = Position {
+            row: min_row,
+            col: min_col,
+        }
+        .offset(&UP);
+
+        curr_pos = top_start;
+        perimeter_locs = HashSet::new();
+        while curr_pos.col <= max_col {
+            let mut prev = false;
+            // Scan right until we have passed the rightmost column position
+            while curr_pos.row <= max_row + 1 {
+                let curr = self.positions.contains(&curr_pos);
+
+                if prev != curr {
+                    match part {
+                        1 => perimeter += 1,
+                        2 => {
+                            if !perimeter_locs.contains(&(curr_pos.offset(&LEFT), curr)) {
+                                perimeter += 1;
+                            }
+                            perimeter_locs.insert((curr_pos, curr));
+                        }
+                        _ => unreachable!(),
+                    }
+                    prev = curr;
+                }
+
+                curr_pos = curr_pos.offset(&DOWN);
+            }
+            curr_pos = curr_pos.offset(&RIGHT);
+            curr_pos.row = top_start.row;
+        }
+
+        perimeter
     }
 }
 
@@ -332,7 +237,7 @@ fn solve_part_1(map: &Map) -> usize {
 
     regions
         .iter()
-        .map(|region| region.get_area() * region.get_perimeter())
+        .map(|region| region.get_area() * region.get_perimeter(1))
         .sum()
 }
 
@@ -360,6 +265,6 @@ fn solve_part_2(map: &Map) -> usize {
 
     regions
         .iter()
-        .map(|region| region.get_area() * region.get_perimeter2())
+        .map(|region| region.get_area() * region.get_perimeter(2))
         .sum()
 }
